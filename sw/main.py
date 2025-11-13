1ee8601796f298580cecc1995b0f41968929e906
zihao <31238371+sileneer@users.noreply.github.com>
2025-11-13 11:03:35 +0000

    updated 1100 on 13/11

--- sw/main.py
+++ sw/main.py
@@ -1,15 +1,22 @@
 from machine import Pin
 from utime import sleep
 from sw.test_motor import Motor
-import sw.test_input
-# line sensor 1 and 2 are at back of car
-# to make sure the car is going straight
-speed_wheel_left = 50
-speed_wheel_right = 50
-LINE_1_PIN = 21
-LINE_2_PIN = 11
-line_1 = Pin(LINE_1_PIN, Pin.IN, Pin.PULL_DOWN)
-line_2 = Pin(LINE_2_PIN, Pin.IN, Pin.PULL_DOWN)
+
+LEFT_WHEEL_SPEED = 50
+RIGHT_WHEEL_SPEED = 50
+RIGHT_PIN_NUMBER = 21
+LEFT_PIN_NUMBER = 11
+
+BASE_SPEED = 40
+SPEED_ADJUSTMENT = 10
+MIN_SPEED = 20
+MAX_SPEED = 60
+
+correction_needed = False  # Flag for interrupt handler
+
+signal_right = Pin(RIGHT_PIN_NUMBER, Pin.IN, Pin.PULL_DOWN)
+signal_left = Pin(LEFT_PIN_NUMBER, Pin.IN, Pin.PULL_DOWN)
+
 def motor_turn_left(speed=30):
     motor_left.Reverse(speed)
     motor_right.Forward(speed)
@@ -25,7 +32,8 @@ def motor_turn_right(speed=30):
     motor_left.off()
     motor_right.off()
 
-def motor_go_straight(speed_left,speed_right, direction="forward", duration=2):
+
+def motor_go_straight(speed_left, speed_right, direction="forward"):
     if direction == "forward":
         motor_left.Forward(speed_left)
         motor_right.Forward(speed_right)
@@ -33,71 +41,76 @@ def motor_go_straight(speed_left,speed_right, direction="forward", duration=2):
         motor_left.Reverse(speed_left)
         motor_right.Reverse(speed_right)
     else:
-        print("Invalid direction. Use 'forward' or 'reverse'.")
-        return
+        raise ValueError("Invalid direction: must be 'forward' or 'reverse'")
+    
 
-    sleep(duration)
-    motor_left.off()
-    motor_right.off()
+
+def line_follower(p):
+    """Interrupt handler for line sensors"""
+    global LEFT_WHEEL_SPEED, RIGHT_WHEEL_SPEED, correction_needed
+    
+    # Read current sensor values
+    sensor_left = signal_left.value()
+    sensor_right = signal_right.value()
+    
+    print("left: ", sensor_left, " right: ", sensor_right)
+    
+    # Adjust speeds based on sensor readings
+    if sensor_right == 1 and sensor_left == 0:
+        # Right sensor on line - car drifting right, turn left
+        LEFT_WHEEL_SPEED = max(BASE_SPEED - SPEED_ADJUSTMENT, MIN_SPEED)
+        RIGHT_WHEEL_SPEED = min(BASE_SPEED + SPEED_ADJUSTMENT, MAX_SPEED)
+        correction_needed = True
+        
+    elif sensor_left == 1 and sensor_right == 0:
+        # Left sensor on line - car drifting left, turn right
+        LEFT_WHEEL_SPEED = min(BASE_SPEED + SPEED_ADJUSTMENT, MAX_SPEED)
+        RIGHT_WHEEL_SPEED = max(BASE_SPEED - SPEED_ADJUSTMENT, MIN_SPEED)
+        correction_needed = True
+        
+    elif sensor_left == 1 and sensor_right == 1:
+        # Both on line - go straight
+        LEFT_WHEEL_SPEED = BASE_SPEED
+        RIGHT_WHEEL_SPEED = BASE_SPEED
+        correction_needed = True
+        
+    else:
+        # Both off line - maintain current speeds
+        pass
 
     
 
 def init():
-    LINE_1_PIN_POWER = 20
-    LINE_2_PIN_POWER = 12
-    # Power the line sensors
-    pin_power_1 = Pin(LINE_1_PIN_POWER, Pin.OUT)
-    pin_power_1.value(1)  # Power on line sensor 1
-    pin_power_2 = Pin(LINE_2_PIN_POWER, Pin.OUT)
-    pin_power_2.value(1)  # Power on line sensor 2
-    line_1.irq(handler=line_follower,trigger=Pin.IRQ_FALLING)
-    line_2.irq(handler=line_follower,trigger=Pin.IRQ_FALLING)
+    # Set up interrupt handlers for line detection
+    signal_right.irq(handler=line_follower, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
+    signal_left.irq(handler=line_follower, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING)
     global motor_left, motor_right
     motor_left = Motor(dirPin=4, PWMPin=5)   # Motor left is controlled from Motor Driv2 #2, which is on GP2/3
     motor_right = Motor(dirPin=7, PWMPin=6)  # Motor right is controlled from Motor Driv2 #3, which is on GP6/7
     
     
 def main():
-    motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
-    # motor_go_straight(speed=50, direction="forward", duration=2)
-    motor_go_straight(speed_wheel_left,speed_wheel_right, direction="reverse", duration=2)
+    global LEFT_WHEEL_SPEED, RIGHT_WHEEL_SPEED, correction_needed
     
- 
-
-
-
-    # keyboard interrupt to stop motors
+    print("Starting line follower")
     
+    # Start moving forward
+    motor_go_straight(LEFT_WHEEL_SPEED, RIGHT_WHEEL_SPEED, direction="forward")
     
-    return
-def line_follower(p):
-    "Interrupt handler"
-    #The interrupte routine that interrupts the main code after and changes the speeds accordingly
-    sensor_left = line_2.value()
-    sensor_right = line_1.value()
-    #To edit the global variables we need this command
-    global speed_wheel_left, speed_wheel_right
-
-    if (sensor_right == 1 and sensor_left ==0):
-        #Add the speed to the left wheel to correct the motion, but by how much?
-        speed_wheel_left += 5
-    elif (sensor_right == 0 and sensor_left == 1):
-        speed_wheel_right += 5
-    #Adjust the speed so that it could correct its path
-    motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
     while True:
-        sensor_left_new = line_1.value()
-        sensor_right_new = line_2.value()
-        if (sensor_left_new == 1 and sensor_left == 0):
-            speed_wheel_left -= 5
-            motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
-            return
-        elif (sensor_right_new == 1 and sensor_right == 0):
-            speed_wheel_right -= 5
-            motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
-            return
+        # Check if interrupt handler set correction flag
+        if correction_needed:
+            # Apply the new speeds calculated by interrupt handler
+            motor_go_straight(LEFT_WHEEL_SPEED, RIGHT_WHEEL_SPEED, direction="forward")
+            correction_needed = False
+        
+        # Small delay to allow system to respond
+        sleep(0.01)
+
+
 if __name__ == "__main__":
     init()
+
     try:
         main()
     except KeyboardInterrupt:
