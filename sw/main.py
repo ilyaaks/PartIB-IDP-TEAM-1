0595af943311382ccc5fccbbec8a534718556940
Illia Aksenchenko <ia471@cam.ac.uk>
2025-11-13 10:18:32 +0000

    13/11

--- sw/main.py
+++ sw/main.py
@@ -4,12 +4,12 @@ from sw.test_motor import Motor
 import sw.test_input
 # line sensor 1 and 2 are at back of car
 # to make sure the car is going straight
-LINE_1_PIN = 0
-LINE_2_PIN = 0
-line_1 = Pin(LINE_1_PIN, Pin.IN, Pin.PULL_DOWN)
-line_2 = Pin(LINE_2_PIN, Pin.IN, Pin.PULL_DOWN)
 speed_wheel_left = 50
 speed_wheel_right = 50
+LINE_1_PIN = 21
+LINE_2_PIN = 11
+line_1 = Pin(LINE_1_PIN, Pin.IN, Pin.PULL_DOWN)
+line_2 = Pin(LINE_2_PIN, Pin.IN, Pin.PULL_DOWN)
 def motor_turn_left(speed=30):
     motor_left.Reverse(speed)
     motor_right.Forward(speed)
@@ -43,17 +43,22 @@ def motor_go_straight(speed_left,speed_right, direction="forward", duration=2):
     
 
 def init():
+    LINE_1_PIN_POWER = 20
+    LINE_2_PIN_POWER = 12
+    # Power the line sensors
+    pin_power_1 = Pin(LINE_1_PIN_POWER, Pin.OUT)
+    pin_power_1.value(1)  # Power on line sensor 1
+    pin_power_2 = Pin(LINE_2_PIN_POWER, Pin.OUT)
+    pin_power_2.value(1)  # Power on line sensor 2
+    line_1.irq(handler=line_follower,trigger=Pin.IRQ_FALLING)
+    line_2.irq(handler=line_follower,trigger=Pin.IRQ_FALLING)
     global motor_left, motor_right
     motor_left = Motor(dirPin=4, PWMPin=5)   # Motor left is controlled from Motor Driv2 #2, which is on GP2/3
     motor_right = Motor(dirPin=7, PWMPin=6)  # Motor right is controlled from Motor Driv2 #3, which is on GP6/7
     
     
 def main():
-    init()
-    line_1.irq(handler=line_follower,trigger=Pin.IRQ_FALLING)
-    line_2.irq(handler=line_follower,trigger=Pin.IRQ_FALLING)
     motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
-    motor_turn_left()
     # motor_go_straight(speed=50, direction="forward", duration=2)
     motor_go_straight(speed_wheel_left,speed_wheel_right, direction="reverse", duration=2)
     
@@ -65,11 +70,13 @@ def main():
     
     
     return
-def line_follower(speed_wheel_left, speed_wheel_right):
+def line_follower(p):
     "Interrupt handler"
     #The interrupte routine that interrupts the main code after and changes the speeds accordingly
-    sensor_left = line_1.value()
-    sensor_right = line_2.value()
+    sensor_left = line_2.value()
+    sensor_right = line_1.value()
+    #To edit the global variables we need this command
+    global speed_wheel_left, speed_wheel_right
 
     if (sensor_right == 1 and sensor_left ==0):
         #Add the speed to the left wheel to correct the motion, but by how much?
@@ -83,9 +90,19 @@ def line_follower(speed_wheel_left, speed_wheel_right):
         sensor_right_new = line_2.value()
         if (sensor_left_new == 1 and sensor_left == 0):
             speed_wheel_left -= 5
+            motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
+            return
         elif (sensor_right_new == 1 and sensor_right == 0):
             speed_wheel_right -= 5
-        motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
-        return
+            motor_go_straight(speed_wheel_left,speed_wheel_right, direction="forward", duration=2)
+            return
 if __name__ == "__main__":
-    main()
+    init()
+    try:
+        main()
+    except KeyboardInterrupt:
+        #Turn everything off
+        motor_left.off()
+        motor_right.off()
+
+
